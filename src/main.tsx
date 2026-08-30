811c5bae44be56eb5d8b1a4ab25a50a20f7cfa70
NikaBakradze <nikabakradze10@gmail.com>
2026-08-31 02:03:37 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { GoogleOAuthProvider } from '@react-oauth/google'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import App from './App.tsx';
+import './index.css';
 
-const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || "529445039101-32tto4o7noirm1vvndnvnl9fl9h0klds.apps.googleusercontent.com";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
       <App />
     </GoogleOAuthProvider>
-  </React.StrictMode>,
-)
+  </StrictMode>,
+);
